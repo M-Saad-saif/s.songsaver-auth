15a7f6df44b7819931501589e83dffb239b6d1ac
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-01-07 20:19:25 +0500

    adding deleteruser and getuser logic in backend

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+// secret key
+const JWT_SECURE = "123456saadsaif123456";
+
+const fetchuser = (req, res, next) => {
+  // get token from header
+  const token = req.header("auth-token");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: "please authenticate with valid token" });
+  }
+
+  try {
+    //   verrifying token
+    const data = jwt.verify(token, JWT_SECURE);
+    req.user = data.user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      error: "Please authenticate using a valid token",
+      details: error.message,
+    });
+  }
+};
+
+module.exports = fetchuser;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchUser");
 
 // secret key
 const JWT_SECURE = "123456saadsaif123456";
@@ -63,5 +64,81 @@ router.post(
 );
 
 //ROUTE 2:  user login : POST '/api/auth/login.  login required
+router.post(
+  "/login",
+  [
+    // validator of backend
+    body("email", "Enter proper email").isEmail(),
+    body("password")
+      .isLength({ min: 2 })
+      .withMessage("must be at least 2 chars long"),
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // checking if the user with no email try to login
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(500).json({ error: "no user found with this Email" });
+      }
+
+      // checking password if uesr put correct password
+      let comparePass = await bcrypt.compare(password, user.password);
+      if (!comparePass) {
+        return res.status(400).json({ error: "login with correct credential" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // giving auth token to user in json form
+      const authtoken = jwt.sign(data, JWT_SECURE);
+      res.json({ token: authtoken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json("Server issue or error occured");
+    }
+  }
+);
+
+//ROUTE 3:  getuser through JWT  : POST '/api/auth/getuser.  login required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    // finding user by ID
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({ error: "error occured" });
+  }
+});
+
+//ROUTE 4:  deleteuser : POST '/api/auth/deleteuser.  login required
+router.delete("/deleteuser", fetchuser, async (req, res) => {
+  try {
+    // finding user by id
+    const userId = req.user.id;
+    const user = await User.findByIdAndDelete(userId).select("-password");
+    
+    // checking if user not found
+    if (!user) {
+      res.status(401).json({ error: "no user found" });
+    }
+
+    res.json({ user, message: "User successfully deleted" });
+  } catch (error) {
+    res.status(401).json({ error: "error occured" });
+  }
+});
 
 module.exports = router;
